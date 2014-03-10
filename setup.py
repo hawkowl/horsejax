a2a70927de3d7c9937388ef84de1ca87c0d44666
HawkOwl <hawkowl@atleastfornow.net>
2014-03-10 13:29:01 +0800

    some twistd hax

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
 from setuptools import setup
 
+# From http://stackoverflow.com/questions/7275295/how-do-i-write-a-setup-py-for-a-twistd-twisted-plugin-that-works-with-setuptools
+try:
+    from setuptools.command import egg_info
+    egg_info.write_toplevel_names
+except (ImportError, AttributeError):
+    pass
+else:
+    def _top_level_package(name):
+        return name.split('.', 1)[0]
+
+    def _hacked_write_toplevel_names(cmd, basename, filename):
+        pkgs = dict.fromkeys(
+            [_top_level_package(k)
+                for k in cmd.distribution.iter_distribution_names()
+                if _top_level_package(k) != "twisted"
+            ]
+        )
+        cmd.write_file("top-level names", filename, '\n'.join(pkgs) + '\n')
+
+    egg_info.write_toplevel_names = _hacked_write_toplevel_names
+
 setup(
     name="horsejax",
     version="0.1",
@@ -13,7 +34,7 @@ setup(
     maintainer="HawkOwl",
     maintainer_email="hawkowl@atleastfornow.net",
     url="https://github.com/hawkowl/horsejax/",
-    packages=["horsejax"],
+    packages=["horsejax", "twisted.plugins"],
     package_data=dict(
         horsejax=["*.json"],
     ),
@@ -21,4 +42,19 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 2.7",
     ],
+    install_requires=[
+        "horsephrase",
+        "haddock"
+    ]
 )
+
+
+# Make Twisted regenerate the dropin.cache, if possible.  This is necessary
+# because in a site-wide install, dropin.cache cannot be rewritten by
+# normal users.
+try:
+    from twisted.plugin import IPlugin, getPlugins
+except ImportError:
+    pass
+else:
+    list(getPlugins(IPlugin))
